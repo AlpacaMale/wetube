56cb96c6940a6980c6b1085b264fa9072d8a69a5
AlpacaMale <alpaca9100@gmail.com>
2025-01-07 10:15:11 +0900

    Route mapping

--- README.md
+++ README.md
@@ -27,3 +27,27 @@ cd wetube
 ```bash
 npm i
 ```
+
+## 라우팅
+
+### globalRouter
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+### userRouter
+
+/users/join
+/users/login
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+### videoRouter
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/vidoes/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -6,14 +6,28 @@ const PORT = 4000;
 
 const app = express();
 
-//declare controller
+// declare controller
 const home = (req, res) => res.send("I love middlewares");
 const login = (req, res) => res.send("login");
 
+// middleware
+app.use(morgan("dev")); // logger
+
 // mapping route to function
-app.use(morgan("dev"));
-app.get("/", home);
-app.get("/login", login);
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+// router setting
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+app.use("/", globalRouter);
 
 // server on
 const handleListening = () =>
