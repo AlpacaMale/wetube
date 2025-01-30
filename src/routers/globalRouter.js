2e0b16fe495758a151c8f94b08d29d127e75ba63
AlpacaMale <alpaca9100@gmail.com>
2025-01-30 20:43:04 +0900

    feat post router

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,13 +1,12 @@
 import express from "express";
 import { join, login } from "../controllers/userController";
-import { trending, search } from "../controllers/videoController";
+import { trending } from "../controllers/videoController";
 
 // mapping route to function
 const globalRouter = express.Router();
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-globalRouter.get("/search", search);
 
 // export file for import in other files
 export default globalRouter;
