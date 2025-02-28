3d8ffd3bfd36725bd46f05a7755ddb7e2028b840
AlpacaMale <alpaca9100@gmail.com>
2025-02-28 15:17:49 +0900

    refactor: move initialization logic to init.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-import "./db";
 // const express = require("express"); ugly code
 import express from "express"; // sexy code
 import morgan from "morgan";
@@ -6,13 +5,7 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
-
 const app = express();
-
-// declare controller
-const home = (req, res) => res.send("I love middlewares");
-const login = (req, res) => res.send("login");
 
 // app setting
 app.set("view engine", "pug");
@@ -27,6 +20,4 @@ app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 app.use("/", globalRouter);
 
-// server on
-const handleListening = () => console.log(`✅ Server listening on http://localhost:${PORT} 🚀`);
-app.listen(PORT, handleListening);
+export default app;
