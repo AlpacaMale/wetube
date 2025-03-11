aa704e2aed1915c5cfbb12f82465fb35af15514c
AlpacaMale <alpaca9100@gmail.com>
2025-03-11 21:59:51 +0900

    Implement url using regex

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -9,7 +9,8 @@ import {
 
 // mapping route to function
 const videoRouter = express.Router();
-videoRouter.get("/:id(\\d+)", watch);
+videoRouter.get("/:id([0-91-f]{24})", watch);
+videoRouter.route("/:id([0-91-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 // export file for import in other files
