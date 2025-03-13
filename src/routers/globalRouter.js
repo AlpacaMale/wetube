7399b62b41a6722384b3c33bceca548065621306
AlpacaMale <alpaca9100@gmail.com>
2025-03-13 14:59:35 +0900

    feat: Implement search video

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { join, login } from "../controllers/userController";
-import { home } from "../controllers/videoController";
+import { home, search } from "../controllers/videoController";
 
 // mapping route to function
 const globalRouter = express.Router();
 globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 // export file for import in other files
 export default globalRouter;
