cca59648a02a06532c6205a9b3adc1d28ecf348a
AlpacaMale <alpaca9100@gmail.com>
2025-03-14 01:29:08 +0900

    chore: Use .env to hide secret and database url

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 import express from "express"; // sexy code
 import session from "express-session";
 import morgan from "morgan";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -18,9 +19,10 @@ app.use(morgan("dev")); // logger
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 app.use(localsMiddleware);
