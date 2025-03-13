1bdae9c3f0332044f0333005ba6a9dfd4e483adb
AlpacaMale <alpaca9100@gmail.com>
2025-03-13 15:10:37 +0900

    feat: Import user model

--- src/init.js
+++ src/init.js
@@ -1,9 +1,11 @@
 import "./db";
 import "./models/Video";
+import "./models/User";
 import app from "./server";
 
 const PORT = 4000;
 
 // server on
-const handleListening = () => console.log(`✅ Server listening on http://localhost:${PORT} 🚀`);
+const handleListening = () =>
+  console.log(`✅ Server listening on http://localhost:${PORT} 🚀`);
 app.listen(PORT, handleListening);
